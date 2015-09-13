364489b643c389c996dfaf9c09ef8bca6321af8c
deadprogram <ron@hybridgroup.com>
2015-09-12 17:16:31 -0700

    Need a thread to communicate with devices

--- src/alarm-clock.cpp
+++ src/alarm-clock.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <sstream>
+#include <thread>
 
 #include <grove.h>
 #include <buzzer.h>
@@ -24,6 +25,23 @@ void message(upm::Jhd1313m1* screen, const std::string& input, const std::size_t
 	screen->setColor(red, green, blue);
 }
 
+void runner(upm::GroveButton* button) {
+	bool wasPressed = false;
+	bool currentlyPressed = false;
+
+	for (;;) {
+		currentlyPressed = button->value();
+		if ( currentlyPressed && ! wasPressed ) {
+			std::cerr << "Pressed" << std::endl;
+		} else if (! currentlyPressed && wasPressed ) {
+			std::cerr << "Released" << std::endl;
+		}
+
+		wasPressed = currentlyPressed;
+		sleep(1);
+	}
+}
+
 int main() {
 	// rotary connected to A0 (analog in)
 	upm::GroveRotary* rotary = new upm::GroveRotary(0);
@@ -37,6 +55,7 @@ int main() {
 	// screen connected to the default I2C bus
 	upm::Jhd1313m1* screen = new upm::Jhd1313m1(0);
 
+	std::thread t1(runner, button);
 
 	crow::SimpleApp app;
 
@@ -45,12 +64,14 @@ int main() {
 		return "Hello world!";
 	});
 
-	app.port(4567).run();
-  /* Code in this loop will run repeatedly
-   */
-//  for (;;) {
-//
-//  }
-//
-//  return 0;
+	app.port(4567).multithreaded().run();
+
+	t1.join();
+
+	delete rotary;
+	delete button;
+	delete buzzer;
+	delete screen;
+
+	return 0;
 }
