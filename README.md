a2236446ce179b499c93cf613d0373d4e328835e
deadprogram <ron@hybridgroup.com>
2015-11-05 15:22:32 -0800

    First pass at watering system CPP example

---
+++ README.md
@@ -0,0 +1,150 @@
+# Watering System
+
+## Introduction
+
+This automatic watering system application is part of a series of how-to Intel� IoT code sample exercises using the Intel� IoT Developer Kit, Intel� Edison development platform, cloud platforms, APIs, and other technologies.
+
+From this exercise, developers will learn how to:
+- Connect the Intel� Edison development platform, a�computing platform designed for prototyping and producing IoT and wearable computing products.
+- Interface with the Intel� Edison platform IO and sensor repository using MRAA and UPM from the Intel� IoT Developer Kit, a complete hardware and software solution to help developers explore the IoT and implement innovative projects.
+- Run this code sample in Intel� XDK IoT Edition, an IDE for�creating new applications that interact with sensors, actuators, and so on, enabling you to get a quick start on developing software for your Intel� Edison or Galileo board.
+- Set up a web application server to store watering system data using Azure Redis Cache* from Microsoft* Azure*, cloud services for connecting IoT solutions including data analysis, machine learning, and a variety of productivity tools to simplify the process of connecting your sensors to the cloud and getting your IoT project up and running quickly.
+- Invoke the services of the Twilio* API for sending SMS messages.
+
+## What It Is
+
+Using an Intel� Edison board, this project lets you create a automatic plant watering system that:
+- turns on and off a water pump based on a configurable schedule;
+- detects if watering system is pumping when expected, by using a water flow sensor;
+- can be accessed with your mobile phone via the built-in web interface to set the watering times;
+- keeps track of watering system, using cloud-based data storage;
+- sends SMS messages to alert if the system if not working as expected.
+
+## How It Works
+
+This watering system has a few extremely useful features.
+Set the watering system schedule using a web page served directly from the Edison itself, using your mobile phone.
+
+It automatically checks the moisture sensor data at periodic intervals, and lets you see that data on the web page.
+
+If the water pump is supposed to be on, but the water flow sensor does not detect that the pumping is talking place as expected, then it sends an SMS to a specified number through Twilio so the watering system can be repaired.
+
+Optionally, it can also log watering system events using the "Intel IoT Example Datastore" running on your own Microsoft Azure account.
+
+## Hardware requirements
+
+Grove* Environment & Agriculture Kit containing:
+
+1. Intel® Edison with an Arduino* breakout board
+2. [Grove* Moisture Sensor](http://iotdk.intel.com/docs/master/upm/node/classes/grovemoisture.html)
+3. [Water Pump](http://www.seeedstudio.com/depot/6V-Mini-Water-Pump-p-1945.html)
+4. [Water Flow Sensor](http://www.seeedstudio.com/depot/G18-Water-Flow-Sensor-p-1346.html)
+5. [Dry-reed Relay](http://iotdk.intel.com/docs/master/upm/node/classes/groverelay.html)
+
+## Software requirements
+
+1. Eclipse*  IoT Edition
+2. Microsoft* Azure* account
+3. Twilio* account
+
+## Adding The Code To Eclipse IoT
+
+You use the Eclipse "Import Wizard" to import an existing project into the workspace as follows:
+
+- From the main menu bar, select "File > Import..."
+![](./../../../images/cpp/cpp-eclipse-menu.png)
+
+- The "Import wizard" dialog will open.
+![](./../../../images/cpp/cpp-eclipse-menu-select-epiw.png)
+
+- Select "General > Existing Project into Workspace" and click on the "Next" button.
+![](./../../../images/cpp/cpp-eclipse-menue-epiw-rootdir.png)
+
+- Choose "Select root directory", then click on the associated "Browse" button to locate the directory that contains the project files.
+![](./../../../images/cpp/cpp-eclipse-menu-select-rootdir.png)
+
+- Under "Projects" select the directory with the project files which you would like to import.
+![](./../../../images/cpp/cpp-eclipse-menue-epiw-rootdir.png)
+- Click on the "Finish" button to import the files into Eclipse.
+
+![](./../../../images/cpp/cpp-eclipse-menu-src-loc.png)
+- Your main .cpp program will now be in your workspace under the src folder.
+
+### Connecting The Grove* Sensors
+
+![](./../../../images/js/watering.jpg)
+
+You need to have a Grove* Shield connected to an Arduino-compatible breakout board, to plug in all the Grove* devices into the Grove* shield. Make sure you have the tiny VCC switch on the Grove* Shield set to "5V".
+
+For this example, you will need to power the Intel Edison with the external power adaptor that came with your starter kit, or else substitute a external 12V 1.5A power supply. You can also use an external battery, such as a 5V USB battery.
+
+In addition, you will need a breadboard and additional 5V power supply to provide power to the pump. Note that you need a separate battery or power supply for the pump. You cannot use the same power supply for both the Intel Edison and pump, so you will need either 2 batteries or 2 power supplies in total.
+
+You will need to use the "Grove Dry-reed Relay" board to connect the "Water Pump". Start by plugging one end of a Grove cable into the "Grove Dry-reed Relay", then connect the other end to the "D4" port on the Grove Shield.
+
+Connect one wire from the pump to the 5V power source reserved for the pump. Connect the other wire from the pump to one of the power connectors on the "Grove Dry-reed Relay" board. Connect the other power connector on the "Grove Dry-reed Relay" board to the ground of the 5V power source reserved for the pump.
+
+Connect the "Water Flow Sensor" by connecting the red wire to the "5V" pin on the Grove shield, the black wire to the GND pin on the Grove Shield, then plug the yellow wire into the "2" pin (aka digital pin 2) on the Grove Shield.
+
+Plug one end of a Grove* cable into the "Grove* Moisture Sensor", and connect the other end to the "A0" port on the Grove* Shield.
+
+### Twilio API Key
+
+To optionally send SMS messages, you need to register for an account and get an API key from the Twilio web site: https://www.twilio.com/
+
+You cannot send SMS messages without obtaining a Twilio API key first. You can still run the example, but without the SMS alerts.
+
+Pass the `TWILIO_ACCT_SID` and `TWILIO_AUTH_TOKEN` to the example program, by modifying the `TWILIO_ACCT_SID` and `TWILIO_AUTH_TOKEN` keys in the run configuration menu -> commands to execute before application
+
+```
+  "TWILIO_ACCT_SID": "YOURAPIKEY"; "TWILIO_AUTH_TOKEN": "YOURTOKEN"
+```
+
+### Microsoft Azure server setup
+
+Optionally, you can store the data generated by this example program in a backend database deployed using Microsoft Azure, Node.js, and a Redis data store.
+
+For information on how to setup your own cloud data server, go to:
+
+https://github.com/intel-iot-devkit/intel-iot-examples-datastore
+
+## Configuring The Example
+
+To configure the example for the optional SMS sending, obtain a key from the Twilio web site as documented above, to the example program, by modifying the `TWILIO_ACCT_SID` and `TWILIO_AUTH_TOKEN` keys in the run configuration menu -> commands to execute before application
+
+```
+  "TWILIO_ACCT_SID": "YOURAPIKEY"; "TWILIO_AUTH_TOKEN": "YOURTOKEN"
+```
+
+To configure the example for the optional Microsoft Azure data store, change the to the example program, by modifying the `TWILIO_ACCT_SID` and `TWILIO_AUTH_TOKEN` keys in the run configuration menu -> commands to execute before application
+```
+  "SERVER": "http://intel-examples.azurewebsites.net/logger/lighting-system"; "AUTH_TOKEN": "s3cr3t"
+```
+
+To configure the example for the both the the SMS and Microsoft Azure datastore, change the to the example program, by modifying the in the run configuration menu -> commands to execute before application `TWILIO_ACCT_SID`, `TWILIO_AUTH_TOKEN`, `SERVER` and `AUTH_TOKEN` keys as follows:
+
+```
+  "TWILIO_ACCT_SID": "YOURAPIKEY"; "TWILIO_AUTH_TOKEN": "YOURTOKEN"; "SERVER": "http://intel-examples.azurewebsites.net/logger/lighting-system"; "AUTH_TOKEN": "s3cr3t"
+```
+### Running The Code On Edison
+
+When you're ready to run the example, you can click on the "Run" icon located in the menubar at the top of the Eclipse editor.
+This will compile the program using the Cross G++ Compiler, link it using the Cross G++ Linker, transfer the binary to the Edison, and then execute it on the Edison itself.
+
+
+### Running The Code On Edison
+
+![](./../../../images/cpp/cpp-run-eclipse.png)
+
+When you're ready to run the example, you can click on the "Run" icon located in the menubar at the top of the Eclipse editor.
+This will compile the program using the Cross G++ Compiler, link it using the Cross G++ Linker, transfer the binary to the Edison, and then execute it on the Edison itself.
+
+![](./../../../images/cpp/cpp-run-eclipse-successful-build.png)
+
+After running the program you should have a similar output as in the image above.
+
+When the program uploads to the Edison board your rgb lcd display will show the current time.
+
+## Regenerating the HTML and CSS
+
+If you make any changes to either the `index.html` or `styles.css` files, you will need to regenerate the hex file used to serve up the assets on via the built-in Crow web server.
---
+++ src/watering-system.cpp
@@ -0,0 +1,425 @@
+#include <string>
+using std::string;
+#include <sstream>
+#include <signal.h>
+
+#include <ctime>
+#include <time.h>
+#include <vector>
+using std::vector;
+
+#include <mraa.hpp>
+#include <grovewfs.h>
+#include <grovemoisture.h>
+
+#include "../lib/restclient-cpp/include/restclient-cpp/restclient.h"
+
+#include "../lib/crow/crow_all.h"
+#include "html.h"
+
+#include "../lib/twilio-cplusplus/Utils.h"
+#include "../lib/twilio-cplusplus/Rest.h"
+#include "../lib/twilio-cplusplus/TwiML.h"
+using namespace twilio;
+const string TWILIO_API_VERSION = "2010-04-01";
+
+using namespace std;
+
+// An individual schedule item for a 1 hour time period
+struct WateringScheduleItem
+{
+  bool on;
+  bool off;
+};
+
+// The entire watering schedule
+struct WateringSchedule
+{
+  WateringScheduleItem schedule[24];
+  bool turnedOn;
+  bool turnedOff;
+
+  // Initialization function
+  WateringSchedule() {
+    turnedOn = false;
+    turnedOff = false;
+
+    for (int i= 0; i < 24; i++) {
+      schedule[i].on = false;
+      schedule[i].off = false;
+    }
+  };
+
+  // Sets the watering schedule for a specific one hour period
+  void set(int hour, bool on, bool off) {
+    schedule[hour].on = on;
+    schedule[hour].off = off;
+  }
+
+  // Useful function used for checking how much time is left before target time
+  double countdown(std::time_t& target) {
+    time_t rawtime;
+    struct tm* timeinfo;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    return std::difftime(mktime(timeinfo), target);
+  }
+
+  // Is it top of the hour?
+  bool it_is_hour(struct tm* timeinfo) {
+    time_t alarmTime ;
+    timeinfo->tm_min = 0;
+    timeinfo->tm_sec = 0;
+    alarmTime = mktime(timeinfo);
+
+    double remaining = countdown(alarmTime);
+
+    if (remaining > 0 && remaining < 5) {
+      return true;
+    } else return false;
+  }
+
+  // Is it time to turn on?
+  bool is_on_time(struct tm* timeinfo) {
+    return schedule[timeinfo->tm_hour].on;
+  }
+
+  // Is it time to turn off?
+  bool is_off_time(struct tm* timeinfo) {
+    return schedule[timeinfo->tm_hour].off;
+  }
+
+  // Render the current watering schedule as JSON
+  crow::json::wvalue renderJSON() {
+    crow::json::wvalue result;
+    for (int i= 0; i < 24; i++) {
+      string iStr = to_string(i);
+      result["data"][iStr]["on"] = schedule[i].on ? 1 : 0;
+      result["data"][iStr]["off"] = schedule[i].off ? 1 : 0;
+    }
+
+    return result;
+  }
+};
+
+// A single data reading for the connected moisture sensor
+struct MoistureDataItem
+{
+  int data;
+  string when;
+
+  // Initialization function
+  MoistureDataItem(int d, string w) {
+    data = d;
+    when = w;
+  };
+
+  string render() {
+    stringstream resultStream;
+    resultStream << "<tr>";
+    resultStream << "<td>" << when << "</td>";
+    resultStream << "<td>" << data << "</td>";
+    resultStream << "</tr>";
+    return resultStream.str();
+  }
+};
+
+// All of the data reads from the moisture sensor
+struct MoistureData
+{
+  std::list<MoistureDataItem> data;
+  int lastReading;
+
+  // Initialization function
+  MoistureData() {
+    lastReading = 0;
+  };
+
+  // Push this new value onto list
+  void add(int val, string timestamp) {
+    MoistureDataItem item(val, timestamp);
+    data.push_front(item);
+    if (data.size() > 20) {data.pop_back();}
+  }
+
+  // Nicely formatted text for the web page
+  string renderText() {
+    stringstream resultStream;
+
+    for (std::list<MoistureDataItem>::iterator it = data.begin(); it != data.end(); ++it) {
+      resultStream << it->render() << '\n';
+    }
+
+    return resultStream.str();
+  }
+};
+
+// Send sms message using Twilio api
+void send_sms() {
+  if (!getenv("TWILIO_SID") || !getenv("TWILIO_TOKEN") ||
+      !getenv("TWILIO_TO") || !getenv("TWILIO_FROM")) {
+    std::cerr << "Twilio not configured." << std::endl;
+    return;
+  }
+
+  string response;
+  vector<Var> vars;
+
+  Rest t(getenv("TWILIO_SID"), getenv("TWILIO_TOKEN"));
+
+  vars.push_back(Var("To", getenv("TWILIO_TO")));
+  vars.push_back(Var("From", getenv("TWILIO_FROM")));
+  vars.push_back(Var("Body", "Watering Alert!"));
+  response = t.request("/" + TWILIO_API_VERSION + "/Accounts/" + getenv("TWILIO_SID") + "/SMS/Messages", "POST", vars);
+  cout << "SMS Sent" << endl;
+  cout << response << endl;
+}
+
+// Log the event to the remote datastore
+void log(const std::string& event) {
+  std::cerr << event << std::endl;
+
+  if (!getenv("SERVER") || !getenv("AUTH_TOKEN")) {
+    std::cerr << "Datastore not configured." << std::endl;
+    return;
+  }
+
+  std::time_t now = std::time(NULL);
+  char mbstr[sizeof "2011-10-08T07:07:09Z"];
+  std::strftime(mbstr, sizeof(mbstr), "%FT%TZ", std::localtime(&now));
+
+  std::stringstream payload;
+  payload << "{\"value\":";
+  payload << "\"" << event << mbstr << "\"}";
+
+  RestClient::headermap headers;
+  headers["X-Auth-Token"] = getenv("AUTH_TOKEN");
+
+  RestClient::response r = RestClient::put(getenv("SERVER"), "text/json", payload.str(), headers);
+
+  cout << "Datastore updated." << endl;
+}
+
+// The hardware devices that the example is going to connect to
+struct Devices
+{
+  upm::GroveWFS* flow;
+  upm::GroveMoisture* moisture;
+  mraa::Gpio* pump;
+
+  int moistureReading = 0;
+  bool turnedOn = false;
+  bool turnedOff = false;
+
+  Devices() {
+  };
+
+  // Initialization function
+  void init() {
+    // water flow sensor to D2
+    flow = new upm::GroveWFS(2);
+
+    // pump attached to D4
+    pump = new mraa::Gpio(4);
+    pump->dir(mraa::DIR_OUT);
+
+    // moisture sensor attached to A1
+    moisture = new upm::GroveMoisture(1);
+  };
+
+  // Cleanup on exit
+  void cleanup() {
+    delete flow;
+    delete pump;
+    delete moisture;
+  }
+
+  // Reads the current value from the moisture sensor
+  int readMoisture() {
+    return moisture->value();
+  }
+
+  // Reads the current value from the flow sensor
+  int readFlow() {
+    return 0; //light->value();
+  }
+
+  // Is the water supposed to be turned on?
+  bool turned_on() {
+    return turnedOn;
+  }
+
+  // Is the water supposed to be turned off?
+  bool turned_off() {
+    return turnedOff;
+  }
+
+  // Turn on the water
+  void turn_on() {
+    if (turnedOn) return;
+    turnedOn = true;
+    turnedOff = false;
+    message("on");
+    log("on");
+  }
+
+  // Turn off the water
+  void turn_off() {
+    if (turnedOff) return;
+    turnedOn = false;
+    turnedOff = true;
+    message("off");
+    log("off");
+  }
+
+  // Displays message to console
+  void message(string msg){
+    cout << msg << endl;
+  }
+};
+
+// The thread that reads the moisture sensor
+void runner(Devices& devices, MoistureData& moistureData) {
+  for (;;)
+  {
+    std::time_t now = std::time(NULL);
+    char mbstr[sizeof "2011-10-08T07:07:09Z"];
+    std::strftime(mbstr, sizeof(mbstr), "%FT%TZ", std::localtime(&now));
+
+    int moistureReading = devices.readMoisture();
+    moistureData.add(moistureReading, mbstr);
+    sleep(10);
+  }
+}
+
+// The thread that reads the flow sensor
+void runner2(Devices& devices) {
+  for (;;)
+  {
+    // int lightReading = devices.readLight();
+    //
+    // if ((lightReading < 2 && devices.turned_on()) ||
+    //     (lightReading > 4 && devices.turned_off())) {
+    //     log("Lighting alert");
+    //     devices.message("Lighting alert");
+    //     send_sms();
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(600000));
+    // } else {
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    // }
+  }
+}
+
+// The thread that runs the scheduled tasks
+void runner3(Devices& devices, WateringSchedule& schedule) {
+  for (;;) {
+    time_t currentTime ;
+    time(&currentTime);
+    struct tm* timeinfo;
+    timeinfo = localtime(&currentTime);
+
+    if (schedule.it_is_hour(timeinfo)) {
+      if (schedule.is_on_time(timeinfo)) {
+        devices.turn_on();
+      } else if (schedule.is_off_time(timeinfo)) {
+        devices.turn_off();
+      }
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  }
+}
+
+Devices devices;
+WateringSchedule schedule;
+MoistureData moistureData;
+
+// Exit handler for program
+void exit_handler(int param)
+{
+  devices.cleanup();
+  exit(1);
+}
+
+// The main function for the example program
+int main()
+{
+  // handles ctrl-c or other orderly exits
+  signal(SIGINT, exit_handler);
+
+  // check that we are running on Galileo or Edison
+  mraa_platform_t platform = mraa_get_platform_type();
+  if ((platform != MRAA_INTEL_GALILEO_GEN1) &&
+      (platform != MRAA_INTEL_GALILEO_GEN2) &&
+      (platform != MRAA_INTEL_EDISON_FAB_C)) {
+      cerr << "ERROR: Unsupported platform" << endl;
+      return MRAA_ERROR_INVALID_PLATFORM;
+  }
+
+  devices.init();
+
+  std::thread t1(runner, ref(devices), ref(moistureData));
+  std::thread t2(runner2, ref(devices));
+  std::thread t3(runner3, ref(devices), ref(schedule));
+
+  crow::SimpleApp app;
+
+  CROW_ROUTE(app, "/")
+  ([](const crow::request& req) {
+    stringstream text;
+    text << index_html;
+
+    string page;
+    page = text.str();
+    string mst;
+    mst = moistureData.renderText();
+    size_t f = page.find("$MOISTUREDATA$");
+    page.replace(f, std::string("$MOISTUREDATA$").length(), mst);
+    return page;
+  });
+
+  CROW_ROUTE(app, "/schedule")
+    .methods("PUT"_method, "GET"_method)
+    ([](const crow::request& req) {
+      if (req.method == "PUT"_method) {
+        auto sched = crow::json::load(req.body);
+        if (!sched)
+           return crow::response(400);
+
+        if (sched.size() != 24) {
+          cout << "Invalid schedule data" << endl;
+        }
+
+        for (int i= 0; i < 24; i++) {
+          string tempI = to_string(i);
+          schedule.set(i, sched[tempI]["on"].b(), sched[tempI]["off"].b());
+        }
+
+        return crow::response("ok");
+      } else {
+        return crow::response(schedule.renderJSON());
+      }
+  });
+
+  CROW_ROUTE(app, "/on")
+  ([]() {
+    devices.turn_on();
+    return crow::response("ok");
+  });
+
+  CROW_ROUTE(app, "/off")
+  ([]() {
+    devices.turn_off();
+    return crow::response("ok");
+  });
+
+  // starts the web server
+  app.port(3000).multithreaded().run();
+
+  // waits forever on the worker threads
+  t1.join();
+  t2.join();
+  t3.join();
+
+  return 0;
+}
