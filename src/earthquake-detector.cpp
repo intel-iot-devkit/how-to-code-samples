1c366dbda60635d09892e77605e2a186b51ecbf0
deadprogram <ron@hybridgroup.com>
2015-11-05 08:41:37 -0800

    Updates for code formatting plus add sigint handlers

--- src/earthquake-detector.cpp
+++ src/earthquake-detector.cpp
@@ -14,146 +14,154 @@ const std::string DEFAULT_LATITUDE = "47.641944";
 const std::string DEFAULT_LONGITUDE = "-122.127222";
 
 std::string latitude() {
-	if (getenv("LATITUDE")) {
-		return getenv("LATITUDE");
-	} else {
-		return DEFAULT_LATITUDE;
-	}
+  if (getenv("LATITUDE")) {
+    return getenv("LATITUDE");
+  } else {
+    return DEFAULT_LATITUDE;
+  }
 }
 
 std::string longitude() {
-	if (getenv("LONGITUDE")) {
-		return getenv("LONGITUDE");
-	} else {
-		return DEFAULT_LONGITUDE;
-	}
+  if (getenv("LONGITUDE")) {
+    return getenv("LONGITUDE");
+  } else {
+    return DEFAULT_LONGITUDE;
+  }
 }
 
 struct Devices
 {
-	upm::MMA7660* accel;
-	upm::Jhd1313m1* screen;
+  upm::MMA7660* accel;
+  upm::Jhd1313m1* screen;
 
-	Devices(){
-	};
+  Devices(){
+  };
 
-	void init() {
-		// accelerometer connected to i2c
-		accel = new upm::MMA7660(MMA7660_I2C_BUS,
-                             	 MMA7660_DEFAULT_I2C_ADDR);
+  void init() {
+    // accelerometer connected to i2c
+    accel = new upm::MMA7660(MMA7660_I2C_BUS,
+                                MMA7660_DEFAULT_I2C_ADDR);
 
-		accel->setModeStandby();
-		accel->setSampleRate(upm::MMA7660::AUTOSLEEP_64);
-		accel->setModeActive();
+    accel->setModeStandby();
+    accel->setSampleRate(upm::MMA7660::AUTOSLEEP_64);
+    accel->setModeActive();
 
-		// screen connected to the default I2C bus
-		screen = new upm::Jhd1313m1(0);
-	};
+    // screen connected to the default I2C bus
+    screen = new upm::Jhd1313m1(0);
+  };
 
-	void cleanup() {
-		delete accel;
-		delete screen;
-	}
+  void cleanup() {
+    delete accel;
+    delete screen;
+  }
 
-	void reset() {
-		message("quakebot ready");
-	}
+  void reset() {
+    message("quakebot ready");
+  }
 
-	void checking() {
-		message("checking...");
-	}
+  void checking() {
+    message("checking...");
+  }
 
-	void warning() {
-		message("Earthquake!");
-	}
+  void warning() {
+    message("Earthquake!");
+  }
 
-	void noquake() {
-		message("No quake.");
-	}
+  void noquake() {
+    message("No quake.");
+  }
 
-	void message(const std::string& input, const std::size_t color = 0x0000ff) {
-		std::size_t red   = (color & 0xff0000) >> 16;
-		std::size_t green = (color & 0x00ff00) >> 8;
-		std::size_t blue  = (color & 0x0000ff);
+  void message(const std::string& input, const std::size_t color = 0x0000ff) {
+    std::size_t red   = (color & 0xff0000) >> 16;
+    std::size_t green = (color & 0x00ff00) >> 8;
+    std::size_t blue  = (color & 0x0000ff);
 
-		std::string text(input);
-		text.resize(16, ' ');
+    std::string text(input);
+    text.resize(16, ' ');
 
-		screen->setCursor(0,0);
-		screen->write(text);
-		screen->setColor(red, green, blue);
-	}
+    screen->setCursor(0,0);
+    screen->write(text);
+    screen->setColor(red, green, blue);
+  }
 
-	void getAcceleration(float* x, float* y, float* z) {
-		accel->getAcceleration(x, y, z);
-	}
+  void getAcceleration(float* x, float* y, float* z) {
+    accel->getAcceleration(x, y, z);
+  }
 };
 
 void verify(Devices* devices) {
-	devices->checking();
+  devices->checking();
 
-	// we'll check for quakes in the last ten minutes
-	std::time_t base = std::time(NULL);
-	struct tm* tm = localtime(&base);
-	tm->tm_min -= 10;
-	time_t next = mktime(tm);
-	char mbstr[sizeof "2011-10-08T07:07:09Z"];
-	std::strftime(mbstr, sizeof(mbstr), "%FT%TZ", std::localtime(&next));
+  // we'll check for quakes in the last ten minutes
+  std::time_t base = std::time(NULL);
+  struct tm* tm = localtime(&base);
+  tm->tm_min -= 10;
+  time_t next = mktime(tm);
+  char mbstr[sizeof "2011-10-08T07:07:09Z"];
+  std::strftime(mbstr, sizeof(mbstr), "%FT%TZ", std::localtime(&next));
 
-	std::stringstream query;
-	query << "http://earthquake.usgs.gov/fdsnws/event/1/query?";
-	query << "format=geojson&";
-	query << "starttime=" << mbstr << "&";
-	query << "latitude=" << latitude() << "&";
-	query << "longitude=" << longitude() << "&";
-	query << "maxradiuskm=500";
+  std::stringstream query;
+  query << "http://earthquake.usgs.gov/fdsnws/event/1/query?";
+  query << "format=geojson&";
+  query << "starttime=" << mbstr << "&";
+  query << "latitude=" << latitude() << "&";
+  query << "longitude=" << longitude() << "&";
+  query << "maxradiuskm=500";
 
-	std::cerr << query.str();
+  std::cerr << query.str();
 
-	RestClient::response r = RestClient::get(query.str());
+  RestClient::response r = RestClient::get(query.str());
 
-	Json::Value root;
-	std::istringstream str(r.body);
-	str >> root;
+  Json::Value root;
+  std::istringstream str(r.body);
+  str >> root;
 
-	const Json::Value features = root["features"];
-	if (features.size() > 0) {
-		devices->warning();
-	} else {
-		devices->noquake();
-	}
+  const Json::Value features = root["features"];
+  if (features.size() > 0) {
+    devices->warning();
+  } else {
+    devices->noquake();
+  }
+}
+
+Devices devices;
+
+// Handles ctrl-c or other orderly exits
+void exit_handler(int param)
+{
+  devices.cleanup();
+  exit(1);
 }
 
 int main()
 {
-	// check that we are running on Galileo or Edison
-	mraa_platform_t platform = mraa_get_platform_type();
-	if ((platform != MRAA_INTEL_GALILEO_GEN1) &&
-		(platform != MRAA_INTEL_GALILEO_GEN2) &&
-		(platform != MRAA_INTEL_EDISON_FAB_C)) {
-		std::cerr << "ERROR: Unsupported platform" << std::endl;
-		return MRAA_ERROR_INVALID_PLATFORM;
-	}
+  signal(SIGINT, exit_handler);
 
-	// create and initialize UPM devices
-	Devices devices;
-	devices.init();
-	devices.reset();
+  // check that we are running on Galileo or Edison
+  mraa_platform_t platform = mraa_get_platform_type();
+  if ((platform != MRAA_INTEL_GALILEO_GEN1) &&
+    (platform != MRAA_INTEL_GALILEO_GEN2) &&
+    (platform != MRAA_INTEL_EDISON_FAB_C)) {
+    std::cerr << "ERROR: Unsupported platform" << std::endl;
+    return MRAA_ERROR_INVALID_PLATFORM;
+  }
 
-	bool motionDetected = false;
-	bool prev = false;
-	float ax = 0, ay = 0, az = 0;
+  // create and initialize UPM devices
+  devices.init();
+  devices.reset();
 
-	for (;;) {
+  bool motionDetected = false;
+  bool prev = false;
+  float ax = 0, ay = 0, az = 0;
+
+  for (;;) {
     devices.getAcceleration(&ax, &ay, &az);
-		motionDetected = (ax > 1.0 || ay > 1.0 || az > 1.0);
+    motionDetected = (ax > 1.0 || ay > 1.0 || az > 1.0);
 
-		if (motionDetected && !prev) { verify(&devices); }
-		prev = motionDetected;
-		sleep(1);
-	}
+    if (motionDetected && !prev) { verify(&devices); }
+    prev = motionDetected;
+    sleep(1);
+  }
 
-	devices.cleanup();
-
-	return MRAA_SUCCESS;
+  return MRAA_SUCCESS;
 }
