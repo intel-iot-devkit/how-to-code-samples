f892bca87732b9eec6d05aafd95b85d8e279e29a
deadprogram <ron@hybridgroup.com>
2015-11-05 08:41:37 -0800

    Updates for code formatting plus add sigint handlers

--- src/smart-stovetop.cpp
+++ src/smart-stovetop.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+
 #include <grove.h>
 #include <yg1006.h>
 #include <otp538u.h>
 #include <GroveSpeaker.h>
+
 #include "../lib/restclient-cpp/include/restclient-cpp/restclient.h"
 #include "../lib/crow/crow_all.h"
 #include "html.h"
@@ -12,142 +14,158 @@
 
 using namespace std;
 
-float myTemp = 1000;//global temp value for app; will change when you use app
+// global temp value for app; will change when you use app
+float myTemp = 1000;
 
-void notify() {//send signal to server
-	if (!getenv("SERVER") && !getenv("AUTH_TOKEN")) {
-		cerr << "Server not configured." << std::endl;
-		return;
-	}
-	RestClient::headermap headers;
-	headers["X-Auth-Token"] = getenv("AUTH_TOKEN");
-	RestClient::response r = RestClient::get(getenv("SERVER"), headers);
-	cerr << r.code << std::endl;
-	cerr << r.body << std::endl;
+// send signal to server
+void notify() {
+  if (!getenv("SERVER") || !getenv("AUTH_TOKEN")) {
+    cerr << "Server not configured." << std::endl;
+    return;
+  }
+
+  RestClient::headermap headers;
+  headers["X-Auth-Token"] = getenv("AUTH_TOKEN");
+  RestClient::response r = RestClient::get(getenv("SERVER"), headers);
+  cerr << r.code << std::endl;
+  cerr << r.body << std::endl;
 }
 
 struct Devices
 {
-	upm::GroveSpeaker* speaker;
-	upm::YG1006* flame;
-	upm::OTP538U* temps;
+  upm::GroveSpeaker* speaker;
+  upm::YG1006* flame;
+  upm::OTP538U* temps;
 
-	Devices(){
-	};
+  Devices(){
+  };
 
-	void init() {
+  void init() {
+    // speaker connected to D5 (digital out)
+    speaker = new upm::GroveSpeaker(5);
 
-	    // speaker connected to D5 (digital out)
-		speaker = new upm::GroveSpeaker(5);
-		// flame sensor on D4
-		flame = new upm::YG1006(4);
-		// Instantiate a OTP538U on analog pins A0 and A1
-		// A0 is used for the Ambient Temperature and A1 is used for the
-		// Object temperature.
-		// only plug ir temp sensor into A0 with this code
-		temps = new upm::OTP538U(0, 1, OTP538U_AREF);
-	}
+    // flame sensor on D4
+    flame = new upm::YG1006(4);
 
-	void cleanup() {
-		delete speaker;
-		delete flame;
-		delete temps;
-	}
+    // Instantiate a OTP538U on analog pins A0 and A1
+    // A0 is used for the Ambient Temperature and A1 is used for the
+    // Object temperature.
+    // only plug ir temp sensor into A0 with this code
+    temps = new upm::OTP538U(0, 1, OTP538U_AREF);
+  }
 
-	void alarm() {//starts the alarm
-		  speaker->playSound('c', true, "high");
-	}
+  void cleanup() {
+    delete speaker;
+    delete flame;
+    delete temps;
+  }
 
-	void senseFlame() {//keeps the flame sensor running
-		for(;;)
-		    {
-		      bool val = flame->flameDetected();
-		      if (val){
-		    	  cout << "Flame detected." << endl;
-		    	  alarm();
-		      }
+  // starts the alarm
+  void alarm() {
+    speaker->playSound('c', true, "high");
+  }
 
-		      else
-		      {
-		        cout << "No flame detected." << endl;
-		        sleep(1);
-		      }
-		    }
-	}
+  // keeps the flame sensor running
+  void senseFlame() {
+    for(;;) {
+      bool val = flame->flameDetected();
+      if (val) {
+        cout << "Flame detected." << endl;
+        alarm();
+      }
 
-	void senseTemp(){
-		//keeps the ir temperature sensor running and checking every minute for object and ambient temp
-		//compares object temp to my temp
-		//sounds alarm if object temp is higher than set temp
-	  for(;;)
-		    {
+      else
+      {
+        cout << "No flame detected." << endl;
+        sleep(1);
+      }
+    }
+  }
 
-		        cout << "Ambient temp: " << std::fixed << setprecision(2)
-		             << temps->ambientTemperature()
-		             << " C, Object temp: " << temps->objectTemperature()
-		             << " C" << endl;
+  // keeps the ir temperature sensor running and checking every minute for object and ambient temp
+  // compares object temp to my temp
+  // sounds alarm if object temp is higher than set temp
+  void senseTemp(){
+    for(;;) {
+      cout << "Ambient temp: " << std::fixed << setprecision(2)
+           << temps->ambientTemperature()
+           << " C, Object temp: " << temps->objectTemperature()
+           << " C" << endl;
 
-		      float temp1 = temps->objectTemperature();
+      float temp1 = temps->objectTemperature();
 
-		      if(temp1 >= myTemp){
-		       	  notify();
-		       	  alarm();
-		      }
-		      else
-		    	  sleep(60);
-		    }
-	}
+      if(temp1 >= myTemp){
+        alarm();
+        notify();
+      }
+      else
+        sleep(60);
+    }
+  }
 };
 
-void runFlame(Devices& devices){//keeps the flame sensor running
-		devices.senseFlame();
-	}
+// keeps the flame sensor running
+void runFlame(Devices& devices) {
+  devices.senseFlame();
+}
 
-void runTemp(Devices& devices){//keeps the ir temperature sensor running and checking every minute for object and ambient temp
-		devices.senseTemp();
-	}
+// keeps the ir temperature sensor running and checking every minute for object and ambient temp
+void runTemp(Devices& devices) {
+  devices.senseTemp();
+}
+
+Devices devices;
+
+// Handles ctrl-c or other orderly exits
+void exit_handler(int param)
+{
+  devices.cleanup();
+  exit(1);
+}
 
 int main()
 {
-	// check that we are running on Galileo or Edison
-	mraa_platform_t platform = mraa_get_platform_type();
-	if ((platform != MRAA_INTEL_GALILEO_GEN1) &&
-		(platform != MRAA_INTEL_GALILEO_GEN2) &&
-		(platform != MRAA_INTEL_EDISON_FAB_C)) {
-		cerr << "ERROR: Unsupported platform" << endl;
-		return MRAA_ERROR_INVALID_PLATFORM;
-	}
-	// create and initialize UPM devices
-	Devices devices;
-	devices.init();
+  signal(SIGINT, exit_handler);
 
-	// start worker thread for device communication
-    thread t1(runTemp, ref(devices));
-    thread t2(runFlame, ref(devices));
-	// define web server & routes
-	crow::SimpleApp app;
+  // check that we are running on Galileo or Edison
+  mraa_platform_t platform = mraa_get_platform_type();
+  if ((platform != MRAA_INTEL_GALILEO_GEN1) &&
+    (platform != MRAA_INTEL_GALILEO_GEN2) &&
+    (platform != MRAA_INTEL_EDISON_FAB_C)) {
+    cerr << "ERROR: Unsupported platform" << endl;
+    return MRAA_ERROR_INVALID_PLATFORM;
+  }
 
-	CROW_ROUTE(app, "/")
-		([](const crow::request& req) {
+  // create and initialize UPM devices
+  devices.init();
 
-        if(req.url_params.get("temp") != nullptr) {
-            double newTemp = boost::lexical_cast<double>(req.url_params.get("temp"));
-            cout << "The value of 'temp' is " <<  newTemp << endl;
-            myTemp = newTemp;
+  // start worker threads for device communication
+  thread t1(runTemp, ref(devices));
+  thread t2(runFlame, ref(devices));
 
-        }
-			stringstream text;
-			text << index_html;
-			return text.str();
-	});
+  // define web server & routes
+  crow::SimpleApp app;
 
-	// start web server
-	app.port(3000).multithreaded().run();
+  CROW_ROUTE(app, "/")
+    ([](const crow::request& req) {
 
-	// all done
-	t1.join();
-    t2.join();
-	devices.cleanup();
+      if(req.url_params.get("temp") != nullptr) {
+        double newTemp = boost::lexical_cast<double>(req.url_params.get("temp"));
+        cout << "The value of 'temp' is " <<  newTemp << endl;
+        myTemp = newTemp;
+      }
 
-	return 0;
+      stringstream text;
+      text << index_html;
+      return text.str();
+  });
+
+  // start web server
+  app.port(3000).multithreaded().run();
+
+  // all done
+  t1.join();
+  t2.join();
+
+  return 0;
 }
