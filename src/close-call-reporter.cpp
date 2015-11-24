f3442e037c64cf676b064622e270dafed7863e55
JustInDevelopment <jlstigman@live.com>
2015-11-24 08:27:37 -0800

    close call
    unfinished was testing gps and getting junk in return

--- src/close-call-reporter.cpp
+++ src/close-call-reporter.cpp
@@ -4,68 +4,67 @@
 #include <unistd.h>
 #include <sstream>
 #include <thread>
-#include <ctime>
 #include <string>
+#include <signal.h>
 
 #include <grove.h>
-#include <jhd1313m1.h>
-#include "biss0001.h"
+#include <ublox6.h>
+#include <rfr359f.h>
 
 #include "../lib/restclient-cpp/include/restclient-cpp/restclient.h"
-#include "../lib/crow/crow_all.h"
-#include "../src/html.h"
 
+const size_t bufferLength = 256;
 
 // The hardware devices that the example is going to connect to
 struct Devices
 {
-
-  upm::Jhd1313m1* screen;
-  upm::BISS0001* motion;
+	upm::RFR359F* dInterupter;
+	upm::Ublox6* nmea;
 
   Devices(){
   };
 
+
   // Initialization function
   void init() {
+	  dInterupter = new upm::RFR359F(2);
+	  nmea = new upm::Ublox6(0);
+	  initPort();
 
-    // screen connected to the default I2C bus
-    screen = new upm::Jhd1313m1(0);
-    // motion sensor on digital D4
-    motion = new upm::BISS0001(4);
   };
 
   // Cleanup on exit
   void cleanup() {
-
-    delete screen;
-    delete motion;
+	  delete dInterupter;
+	  delete nmea;
   }
 
-  // Display a message on the LCD
-  void message(const std::string& input, const std::size_t color = 0x0000ff) {
-    std::size_t red   = (color & 0xff0000) >> 16;
-    std::size_t green = (color & 0x00ff00) >> 8;
-    std::size_t blue  = (color & 0x0000ff);
-
-    std::string text(input);
-    text.resize(16, ' ');
-
-    screen->setCursor(0,0);
-    screen->write(text);
-    screen->setColor(red, green, blue);
+  int initPort(){
+	  if (!nmea->setupTty(B115200))
+	    {
+	      std::cerr << "Failed to setup tty port parameters" << std::endl;
+	      return 1;
+	    }
   }
-  void detect(){
-	  bool val = motion->value();
-	  std::string msg = "Person arrived at: ";
-	  if(val){
-		  message("Motion Detected", 0xff00ff);
-		  notify(msg);
-	  }
-	  else{
-		  message("No motion Detected");
-	  }
-  }
+
+  void gpsInfo(){
+	  char nmeaBuffer[bufferLength];
+	      // we don't want the read to block in this example, so always
+	      // check to see if data is available first.
+      int rv = nmea->readData(nmeaBuffer, bufferLength);
+
+	      if (nmea->dataAvailable())
+	        {
+	          if (rv > 0)
+	            write(1, nmeaBuffer, rv);
+	          sleep(1);
+	          if (rv < 0) // some sort of read error occured
+	            {
+	              std::cerr << "Port read error." << std::endl;
+	            }
+
+	        }
+  	  }
 
   void notify(std::string message) {
     if (!getenv("SERVER") || !getenv("AUTH_TOKEN")) {
@@ -88,12 +87,25 @@ struct Devices
     std::cout << r.body << std::endl;
   }
 
+  void check_object_detected(){
+
+      bool isDetected = dInterupter->objectDetected();
+      if (isDetected){
+        std::cout << "Object detected" << std::endl;
+      }
+      else{
+        std::cout << "Area is clear" << std::endl;
+      }
+      sleep(1);
+    }
+
+
 };
 
 // Function called by worker thread for device communication
 void runner(Devices& devices) {
   for (;;) {
-	  	  devices.detect();
+	  devices.gpsInfo();
       }
     }
 Devices devices;
@@ -123,32 +135,10 @@ int main() {
   devices.init();
 
   // start worker thread for device communication
-  std::thread t1(runner, std::ref(devices));
-
-  // define web server & routes
-  crow::SimpleApp app;
-
-  CROW_ROUTE(app, "/")
-  ([](const crow::request& req) {
-
-    std::stringstream text;
-    text << index_html;
-    return text.str();
-  });
-
-  CROW_ROUTE(app, "/alarm")
-  .methods("GET"_method)
-  ([](const crow::request& req) {
-      if (req.method == "POST"_method) {
-    	  //check code here
-      }
-      return crow::response("OK");
-  });
-  // start web server
-  app.port(3000).multithreaded().run();
+  //std::thread t1(runner, std::ref(devices));
 
   // wait forever for the thread to exit
-  t1.join();
+  //t1.join();
 
   return MRAA_SUCCESS;
 }
