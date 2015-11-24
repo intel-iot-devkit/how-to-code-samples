3d7feede5d134e781f6611766d5aa0fe859e72da
deadprogram <ron@hybridgroup.com>
2015-11-24 08:58:08 -0800

    Able to read NMEA data from GPS

--- src/close-call-reporter.cpp
+++ src/close-call-reporter.cpp
@@ -18,53 +18,48 @@ const size_t bufferLength = 256;
 // The hardware devices that the example is going to connect to
 struct Devices
 {
-	upm::RFR359F* dInterupter;
-	upm::Ublox6* nmea;
+  upm::RFR359F* detector;
+  upm::Ublox6* nmea;
 
   Devices(){
   };
 
-
   // Initialization function
   void init() {
-	  dInterupter = new upm::RFR359F(2);
-	  nmea = new upm::Ublox6(0);
-	  initPort();
-
+    detector = new upm::RFR359F(2);
+    nmea = new upm::Ublox6(0);
+    initPort();
   };
 
   // Cleanup on exit
   void cleanup() {
-	  delete dInterupter;
-	  delete nmea;
+    delete detector;
+    delete nmea;
   }
 
   int initPort(){
-	  if (!nmea->setupTty(B115200))
-	    {
-	      std::cerr << "Failed to setup tty port parameters" << std::endl;
-	      return 1;
-	    }
+    if (!nmea->setupTty(B9600)) {
+      std::cerr << "Failed to setup tty port parameters" << std::endl;
+      return 1;
+    }
+    return 0;
   }
 
-  void gpsInfo(){
-	  char nmeaBuffer[bufferLength];
-	      // we don't want the read to block in this example, so always
-	      // check to see if data is available first.
+  void gps_info(){
+    char nmeaBuffer[bufferLength];
+
+    if (nmea->dataAvailable()) {
       int rv = nmea->readData(nmeaBuffer, bufferLength);
+      if (rv > 0) {
+        write(1, nmeaBuffer, rv);
+        std::cout << nmeaBuffer << std::endl;
+      }
 
-	      if (nmea->dataAvailable())
-	        {
-	          if (rv > 0)
-	            write(1, nmeaBuffer, rv);
-	          sleep(1);
-	          if (rv < 0) // some sort of read error occured
-	            {
-	              std::cerr << "Port read error." << std::endl;
-	            }
-
-	        }
-  	  }
+      if (rv < 0) {
+        std::cerr << "Port read error." << std::endl;
+      }
+    }
+  }
 
   void notify(std::string message) {
     if (!getenv("SERVER") || !getenv("AUTH_TOKEN")) {
@@ -88,26 +83,23 @@ struct Devices
   }
 
   void check_object_detected(){
-
-      bool isDetected = dInterupter->objectDetected();
-      if (isDetected){
-        std::cout << "Object detected" << std::endl;
-      }
-      else{
-        std::cout << "Area is clear" << std::endl;
-      }
-      sleep(1);
+    if (detector->objectDetected()) {
+      std::cout << "Object detected" << std::endl;
+    } else {
+      std::cout << "Area is clear" << std::endl;
     }
-
-
+    sleep(1);
+  }
 };
 
 // Function called by worker thread for device communication
 void runner(Devices& devices) {
   for (;;) {
-	  devices.gpsInfo();
-      }
-    }
+    devices.gps_info();
+    usleep(100000);
+  }
+}
+
 Devices devices;
 
 // Exit handler for program
@@ -135,10 +127,10 @@ int main() {
   devices.init();
 
   // start worker thread for device communication
-  //std::thread t1(runner, std::ref(devices));
+  std::thread t1(runner, std::ref(devices));
 
   // wait forever for the thread to exit
-  //t1.join();
+  t1.join();
 
   return MRAA_SUCCESS;
 }
