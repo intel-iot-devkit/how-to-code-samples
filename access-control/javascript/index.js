f3e85f9cadbc44451c3faa58eb77a808452d5b2a
deadprogram <ron@hybridgroup.com>
2016-04-11 15:30:44 -0700

    Remove functions already moved to external files
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- access-control/javascript/index.js
+++ access-control/javascript/index.js
@@ -47,54 +47,6 @@ function log(event) {
   mqtt.log(config, payload);
 }
 
-// Store data in the remote datastore
-function logDatastore(payload) {
-  if (!config.SERVER || !config.AUTH_TOKEN) {
-    return;
-  }
-
-  function callback(err, res) {
-    if (err) {
-      return console.error("err:", err);
-    } else {
-      return console.log("Saved to datastore:", payload);
-    }
-  }
-
-  request
-    .put(config.SERVER)
-    .set("X-Auth-Token", config.AUTH_TOKEN)
-    .send(payload)
-    .end(callback);
-}
-
-// Publish data to MQTT server
-function logMQTT(payload) {
-  if (!config.MQTT_SERVER || !config.MQTT_CLIENTID) {
-    return;
-  }
-
-  var data = {d: payload}
-  var options = {
-    clientId: config.MQTT_CLIENTID,
-    username: config.MQTT_USERNAME,
-    password: config.MQTT_PASSWORD
-  };
-
-  if (config.MQTT_CERT && config.MQTT_KEY) {
-    options.cert = fs.readFileSync(config.MQTT_CERT);
-    options.key = fs.readFileSync(config.MQTT_KEY);
-  }
-
-  console.log("Connecting to MQTT server...");
-  var client  = mqtt.connect(config.MQTT_SERVER, options);
-  client.on('connect', function () {
-    client.publish(config.MQTT_TOPIC, JSON.stringify(data));
-    console.log("MQTT message published:", data);
-    client.end();
-  });
-}
-
 // Displays a message on the RGB LED
 function message(string) {
   // pad string to avoid display issues
--- doorbell/javascript/index.js
+++ doorbell/javascript/index.js
@@ -53,33 +53,6 @@ function increment() {
   mqtt.increment(config);
 }
 
-// Publish data to MQTT server
-function incrementMQTT() {
-  if (!config.MQTT_SERVER || !config.MQTT_CLIENTID) {
-    return;
-  }
-
-  var data = {d: {count: 1}};
-  var options = {
-    clientId: config.MQTT_CLIENTID,
-    username: config.MQTT_USERNAME,
-    password: config.MQTT_PASSWORD
-  };
-
-  if (config.MQTT_CERT && config.MQTT_KEY) {
-    options.cert = fs.readFileSync(config.MQTT_CERT);
-    options.key = fs.readFileSync(config.MQTT_KEY);
-  }
-
-  console.log("Connecting to MQTT server...");
-  var client  = mqtt.connect(config.MQTT_SERVER, options);
-  client.on('connect', function () {
-    client.publish(config.MQTT_TOPIC, JSON.stringify(data));
-    console.log("MQTT message published:", data);
-    client.end();
-  });
-}
-
 // Logs a doorbell ring, prints a message to the display,
 // and rings the buzzer
 function dingdong() {
