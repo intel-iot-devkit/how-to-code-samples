0932f297d5046d705ae6b8ee11ac5556bbc996bc
Noel Eck <noel.eck@intel.com>
2016-04-26 12:11:56 -0700

    doc: Replaced various quotation encodings in file headers
    Small change - standardized encoding for quotations on "User" in
    file headers.  There were 2 different non-utf8 encodings around the
    word User which has a possibility of causing complaints with some text
    tools.  Example, Ubuntu greb would NOT search these files by default
    since the non-utf8 encodings flag the file as binary.
    
    Replaced all instances with "User".
    
    Signed-off-by: Noel Eck <noel.eck@intel.com>

--- air-quality-sensor/cpp/src/air-quality-sensor.cpp
+++ air-quality-sensor/cpp/src/air-quality-sensor.cpp
@@ -1,7 +1,7 @@
 /*
 * Copyright (c) 2015-2016 Intel Corporation.
 *
-* Permission is hereby granted, free of charge, to any person (“User”) obtaining
+* Permission is hereby granted, free of charge, to any person ("User") obtaining
 * a copy of this software and associated documentation files (the
 * "Software"), to deal in the Software without restriction, including
 * without limitation the rights to use, copy, modify, merge, publish,
