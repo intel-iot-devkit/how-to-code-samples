6557db685f291ca129bc03281025ac7340221325
Erik Medina <erik@snowcrabsoftware.com>
2016-11-08 22:53:56 -0800

    [py/doorbell] Add base python project.

---
+++ doorbell/python/board.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2015 - 2016 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from abc import ABCMeta, abstractmethod
+
+from event_emitter import EventEmitter
+
+from scheduler import scheduler, ms
+
+class Board(object):
+
+    """
+    Base class for Board hardware implementations
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        
+        self.emitter = EventEmitter()
+
+        self.hardware_event_job = scheduler.add_job(self.update_hardware_state, "interval", seconds=ms(100), coalesce=True, max_instances=1)
+
+    def trigger_hardware_event(self, event, *args, **kwargs):
+
+        """
+        Signal hardware event.
+        """
+
+        self.emitter.emit(event, *args, **kwargs)
+
+    def add_event_handler(self, event, handler, once=False):
+
+        """
+        Add hardware event handler.
+        """
+        
+        if once:
+            self.emitter.once(event, handler)
+        else:
+            self.emitter.on(event, handler)
+    
+    def remove_event_handler(self, event, handler):
+
+        """
+        Remove hardware event handler.
+        """
+
+        self.emitter.remove(event, handler)
+
+    @abstractmethod
+    def update_hardware_state(self): pass
---
+++ doorbell/python/drobot.py
@@ -0,0 +1,65 @@
+# Copyright (c) 2015 - 2016 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from pyupm_i2clcd import SAINSMARTKS
+
+from events import scheduler, emitter, ms
+from board import Board
+
+class DfrobotBoard(Board):
+
+    """
+    Board class for drobot hardware.
+    """
+
+    def __init__(self, config):
+
+        super(GroveBoard, self).__init__()
+        
+        self.screen = Jhd1313m1(8, 9, 4, 5, 6, 7, 0)
+    
+    def update_hardware_state(self):
+
+        """
+        Update hardware state.
+        """
+
+        pass
+
+    # hardware functions
+    def write_message(self, message, line=0):
+
+        """
+        Write message to LCD screen.
+        """
+
+        message = message.ljust(16)
+        self.screen.setCursor(line, 0)
+        self.screen.write(message)
+
+    def change_background(self, color):
+        
+        """
+        Change LCD screen background color.
+        No effect on the dfrobot.
+        """
+        
+        pass
