c8906ef33c85b76bc1c4d30c3138d92712917161
Erik Medina <erik@snowcrabsoftware.com>
2016-11-18 21:30:05 -0800

    [py/close-call-reporter] Add python base project.

---
+++ close-call-reporter/python/constants/config.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2015 - 2016 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# app specific
+BOARD = "board"
+
+# MQTT server
+MQTT_SERVER = "MQTT_SERVER"
+MQTT_PORT = "MQTT_PORT"
+MQTT_CLIENTID = "MQTT_CLIENTID"
+MQTT_USERNAME = "MQTT_USERNAME"
+MQTT_PASSWORD = "MQTT_PASSWORD"
+MQTT_CERT = "MQTT_CERT"
+MQTT_KEY = "MQTT_KEY"
+MQTT_TOPIC = "MQTT_TOPIC"
+
+# remote data store
+SERVER = "SERVER"
+AUTH_TOKEN = "AUTH_TOKEN"
