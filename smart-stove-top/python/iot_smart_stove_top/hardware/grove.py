d9cb895aa5ccc8b1d3b4687d5d3d43cfc385256e
Erik Medina <erik@snowcrabsoftware.com>
2016-12-27 16:30:38 -0800

    [py/smart-stove-top] Add core project logic.

---
+++ smart-stove-top/python/iot_smart_stove_top/hardware/grove.py
@@ -0,0 +1,99 @@
+# Copyright (c) 2015 - 2016 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from __future__ import print_function
+from upm.pyupm_speaker import Speaker
+from upm.pyupm_otp538u import OTP538U
+from upm.pyupm_yg1006 import YG1006
+from mraa import addSubplatform, GENERIC_FIRMATA
+from ..config import HARDWARE_CONFIG, KNOWN_PLATFORMS
+from .board import Board, PinMappings
+from .events import TEMP_READING, FLAME_DETECTED
+
+class GroveBoard(Board):
+
+    """
+    Board class for Grove hardware.
+    """
+
+    def __init__(self):
+
+        super(GroveBoard, self).__init__()
+
+        # pin mappings
+        self.pin_mappings = PinMappings(
+            speaker_pin=5,
+            flame_pin=4,
+            temp_obj_pin=1,
+            temp_ambient_pin=0,
+            i2c_bus=6,
+            reference_voltage=5.0
+        )
+
+        if HARDWARE_CONFIG.platform == KNOWN_PLATFORMS.firmata:
+            addSubplatform(GENERIC_FIRMATA, "/dev/ttyACM0")
+            self.pin_mappings += 512
+            self.pin_mappings.i2c_bus = 512
+            self.pin_mappings.reference_voltage = 3.3
+
+        self.speaker = Speaker(self.pin_mappings.speaker_pin)
+        self.flame = YG1006(self.pin_mappings.flame_pin)
+        self.temp = OTP538U(
+            self.pin_mappings.temp_ambient_pin,
+            self.pin_mappings.temp_obj_pin,
+            self.pin_mappings.reference_voltage
+        )
+
+
+    def update_hardware_state(self):
+
+        """
+        Update hardware state.
+        """
+
+        obj_temp = self.read_obj_temp()
+        self.trigger_hardware_event(TEMP_READING, obj_temp)
+
+        flame_detected = self.detect_flame()
+        self.trigger_hardware_event(FLAME_DETECTED, flame_detected)
+
+    # hardware functions
+    def read_obj_temp(self):
+
+        return self.temp.objectTemp()
+
+    def detect_flame(self):
+
+        return self.flame.flameDetected()
+
+    def play_temp(self):
+
+        self.speaker.playSound("a", True, "low")
+        self.speaker.playSound("c", True, "low")
+        self.speaker.playSound("d", True, "low")
+        self.speaker.playSound("b", False, "low")
+
+    def play_flame(self):
+
+        for _ in range(0, 10):
+            self.speaker.playSound("a", True, "high")
+            self.speaker.playSound("c", True, "high")
+            self.speaker.playSound("g", True, "med")
---
+++ smart-stove-top/python/iot_smart_stove_top/runner.py
@@ -0,0 +1,127 @@
+# Copyright (c) 2015 - 2016 Intel Corporation.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from __future__ import print_function
+from datetime import datetime, timedelta
+from importlib import import_module
+from pkg_resources import resource_filename
+from bottle import Bottle, static_file, request
+from .config import HARDWARE_CONFIG, APP_CONFIG
+from .log import log
+
+class Runner(object):
+
+    def __init__(self):
+
+        self.project_name = "Smart Stove Top"
+
+        board_name = HARDWARE_CONFIG.kit
+        board_module = "{0}.hardware.{1}".format(__package__, board_name)
+        board_class_name = "{0}Board".format(board_name.capitalize())
+        self.board = getattr(import_module(board_module), board_class_name)()
+
+        # setup HTTP server
+        self.server = Bottle()
+        self.server.route("/", callback=self.serve_index)
+        self.server.route("/temp.json", callback=self.serve_temp)
+
+        # hardware state
+        self.temp_triggered = False
+        self.flame_triggered = False
+
+        self.log_triggered = False
+        self.last_log = datetime.utcnow()
+        self.log_interval = timedelta(seconds=1)
+
+        self.target_temp = APP_CONFIG.target_temp
+
+    def start(self):
+
+        """
+        Start runner.
+        """
+
+        self.server.run(
+            host="0.0.0.0",
+            port=3000
+        )
+
+    # hardware methods
+
+    def update_temp(self, temp):
+
+        should_trigger = temp >= self.target_temp
+
+        if should_trigger and not self.temp_triggered:
+            print("Temperature alarm triggered.")
+            self.temp_triggered = True
+            self.log_triggered = True
+            self.board.play_temp()
+
+        if self.log_triggered:
+            log_delta = self.last_log - datetime.utcnow()
+            if log_delta >= self.log_interval:
+                log(temp)
+
+        if not should_trigger and self.temp_triggered:
+            self.temp_triggered = False
+
+    def update_flame(self, flame_detected):
+
+        should_trigger = flame_detected is True
+
+        if should_trigger and not self.flame_triggered:
+            print("Flame alarm triggered.")
+            self.flame_triggered = True
+            self.board.play_flame()
+
+        if not should_trigger and self.flame_triggered:
+            self.flame_triggered = False
+
+    # server methods
+
+    def serve_index(self):
+
+        """
+        Serve the 'index.html' file.
+        """
+
+        if {"temp"} <= set(request.query):
+
+            print("Process temp query parameter: {0}.".format(dict(request.query)))
+            self.target_temp = int(request.query.get("temp"))
+
+        resource_package = __name__
+        resource_path = "index.html"
+        package_root = resource_filename(resource_package, "")
+        return static_file(resource_path, root=package_root)
+
+    def serve_temp(self):
+
+        """
+        Serve 'temp.json' data.
+        """
+
+        payload = {
+            "temp": self.target_temp
+        }
+
+        return payload
