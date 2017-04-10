e215a6bd65b2d554c9f3c20995771f99988c4dea
Erik Medina <erik@snowcrabsoftware.com>
2017-04-09 21:13:51 -0700

    [py/predix] Update watering system with Predix support.

--- watering-system/python/iot_watering_system/__main__.py
+++ watering-system/python/iot_watering_system/__main__.py
@@ -20,10 +20,13 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from __future__ import print_function, division
-from time import sleep
-from signal import SIGINT, signal
-from atexit import register as register_exit
+from os import environ
+from twisted.internet import reactor
+from certifi import where as locate_trust_store
 from .runner import Runner
+
+# OPENSSL workaround to bootstrap certificate trust store
+environ["SSL_CERT_FILE"] = locate_trust_store()
 
 def main():
 
@@ -31,25 +34,11 @@ def main():
     Start main function.
     """
 
-    def signal_handler(signum, frame):
-        raise SystemExit
-
-    def exit_handler():
-        print("exiting.")
-        exit(0)
-
-    register_exit(exit_handler)
-    signal(SIGINT, signal_handler)
-
     runner = Runner()
     print("Running {0} example.".format(runner.project_name))
     runner.start()
 
-    try:
-        signal.pause()
-    except AttributeError:
-        while True:
-            sleep(0.5)
+    reactor.run()
 
 if __name__ == "__main__":
     main()
